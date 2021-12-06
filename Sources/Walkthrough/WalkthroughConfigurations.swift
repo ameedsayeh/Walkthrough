2edd51f769a67b3bb4ed8d767c9ec12a898eade5
Ameed Sayeh <ameed.iyad.sayeh@gmail.com>
2021-12-06 15:33:38 +0200

    public access fixed

--- Sources/Walkthrough/WalkthroughConfigurations.swift
+++ Sources/Walkthrough/WalkthroughConfigurations.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 public struct WalkthroughConfigurations {
-    
+
     var overlayColor: UIColor = UIColor.black.withAlphaComponent(0.5)
     var animationDuration: TimeInterval = 0.3
     var animationTypes: UIView.AnimationOptions = [.transitionCrossDissolve]
+
+    public init(overlayColor: UIColor = UIColor.black.withAlphaComponent(0.5),
+                animationDuration: TimeInterval = 0.3,
+                animationTypes: UIView.AnimationOptions = [.transitionCrossDissolve]) {
+
+        self.overlayColor = overlayColor
+        self.animationDuration = animationDuration
+        self.animationTypes = animationTypes
+    }
 }
 
 #endif
